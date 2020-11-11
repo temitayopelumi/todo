d151814f6bab9e383f60fa8bf139b44c20e1ba2a
olalekan temitayo <olalekantemitayo7@gmail.com>
2020-11-11 18:56:02 +0100

    todo

---
+++ task/static/js/styles.js
@@ -0,0 +1,36 @@
+(function($) {
+    'use strict';
+    $(function() {
+    var todoListItem = $('.todo-list');
+    var todoListInput = $('.todo-list-input');
+    $('.todo-list-add-btn').on("click", function(event) {
+    event.preventDefault();
+    
+    var item = $(this).prevAll('.todo-list-input').val();
+    
+    if (item) {
+    todoListItem.append("
+        " + item + "
+    ");
+    todoListInput.val("");
+    }
+    
+    });
+    
+    todoListItem.on('change', '.checkbox', function() {
+    if ($(this).attr('checked')) {
+    $(this).removeAttr('checked');
+    } else {
+    $(this).attr('checked', 'checked');
+    }
+    
+    $(this).closest("li").toggleClass('completed');
+    
+    });
+    
+    todoListItem.on('click', '.remove', function() {
+    $(this).parent().remove();
+    });
+    
+    });
+    })(jQuery);
